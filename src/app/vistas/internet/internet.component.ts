4c0fad52fbbdcbf9a5823497345e9022ea58552d
AlejoMontoya <montoyavictoriajuanalejandro@gmail.com>
2024-09-10 17:04:20 -0500

    mi primer commit

---
+++ src/app/vistas/internet/internet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-internet',
+  standalone: true,
+  imports: [],
+  templateUrl: './internet.component.html',
+  styleUrl: './internet.component.scss'
+})
+export class InternetComponent {
+
+}
